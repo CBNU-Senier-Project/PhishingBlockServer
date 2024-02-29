f31494ef74531a435a6a363c6213fc17e9bc8341
sso9594 <sso9594@naver.com>
2024-02-29 19:44:22 +0900

    feat: Controller 추가 및 이메일 체크, 회원가입 메소드 구현
    연관 이슈 : #8

---
+++ src/auth-service/src/main/java/com/phishing/authservice/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.phishing.authservice.controller;
+
+import com.phishing.authservice.dto.request.SignUpRequest;
+import com.phishing.authservice.service.AuthService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+@Validated
+public class AuthController {
+    private final AuthService authService;
+
+    @GetMapping("/users/check")
+    public ResponseEntity<?> checkEmail(@RequestParam @Valid @NotNull String email) {
+        authService.checkEmail(email);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signUp(@RequestBody @Valid SignUpRequest request) {
+        authService.signUp(request);
+        return ResponseEntity.ok().build();
+    }
+}

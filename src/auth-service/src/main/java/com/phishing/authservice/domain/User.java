684b8c720ef6d0a198f257157f24a6b142c4304e
sso9594 <sso9594@naver.com>
2024-02-29 17:09:36 +0900

    feat: User entity 추가

---
+++ src/auth-service/src/main/java/com/phishing/authservice/domain/User.java
@@ -0,0 +1,30 @@
+package com.phishing.authservice.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email", nullable = false, length = 50)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 20)
+    private String password;
+
+    @Column(name = "nickname", nullable = false, length = 15)
+    private String nickname;
+
+    @Column(name = "phnum", nullable = false, length = 20)
+    private String phnum;
+
+    @Column(name = "role", nullable = false, length = 10)
+    private UserRole role;
+}

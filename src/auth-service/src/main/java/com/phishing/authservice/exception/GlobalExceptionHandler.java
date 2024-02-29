63b9cd35352cd8b6ea8b2a3dd4dba235150000f0
sso9594 <sso9594@naver.com>
2024-02-29 19:42:16 +0900

    feat: Exception Handling을 위한 GlobalExceptionHandler 클래스 추가
    연관 이슈 : #8

---
+++ src/auth-service/src/main/java/com/phishing/authservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.phishing.authservice.exception;
+
+import com.phishing.authservice.exception.exceptions.DuplicateEmailException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DuplicateEmailException.class)
+    protected ResponseEntity<?> handleDuplicateEmailException(DuplicateEmailException e) {
+        log.error(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}

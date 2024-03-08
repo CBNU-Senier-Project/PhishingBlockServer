38b2b5ab79800ec08b6d252b002f9e69076603ea
sso9594 <sso9594@naver.com>
2024-03-08 18:23:46 +0900

    refactor: UserRole enum 간소화
    연관 이슈 : #12

--- src/auth-service/src/main/java/com/phishing/authservice/domain/UserRole.java
+++ src/auth-service/src/main/java/com/phishing/authservice/domain/UserRole.java
@@ -1,15 +1,6 @@
 package com.phishing.authservice.domain;
 
-import lombok.Getter;
-
-@Getter
 public enum UserRole {
-    USER("ROLE_USER"),
-    ADMIN("ROLE_ADMIN");
-
-    private String type;
-
-    UserRole(String type) {
-        this.type = type;
-    }
+    USER,
+    ADMIN
 }

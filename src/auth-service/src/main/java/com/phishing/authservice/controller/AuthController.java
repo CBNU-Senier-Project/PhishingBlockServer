97e83ad4e2c56ed4bee8ab3f3ade2a72b7c905a9
sso9594 <sso9594@naver.com>
2024-03-11 10:20:19 +0900

    feat: controller 이메일 인증 메소드 추가
    연관 이슈: #14

--- src/auth-service/src/main/java/com/phishing/authservice/controller/AuthController.java
+++ src/auth-service/src/main/java/com/phishing/authservice/controller/AuthController.java
@@ -2,7 +2,9 @@ package com.phishing.authservice.controller;
 
 import com.phishing.authservice.dto.request.SignInRequest;
 import com.phishing.authservice.dto.request.SignUpRequest;
+import com.phishing.authservice.dto.request.VerifyEmailRequest;
 import com.phishing.authservice.service.AuthService;
+import com.phishing.authservice.service.MailService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
@@ -16,10 +18,23 @@ import org.springframework.web.bind.annotation.*;
 @Validated
 public class AuthController {
     private final AuthService authService;
+    private final MailService mailService;
 
     @GetMapping("/users/check")
     public ResponseEntity<?> checkEmail(@RequestParam @Valid @NotNull String email) {
         authService.checkEmail(email);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/email/send")
+    public ResponseEntity<?> sendMail(@RequestParam @Valid @NotNull String email) {
+        mailService.sendMail(email);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/email/verify")
+    public ResponseEntity<?> verifyMail(@RequestBody @Valid VerifyEmailRequest request) {
+        mailService.verifyMail(request.email(), request.authCode());
         return ResponseEntity.ok().build();
     }
 

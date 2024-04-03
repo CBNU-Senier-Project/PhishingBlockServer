77a54dbd3d83d7c2df931a49c2f967728f3da49d
sso9594 <sso9594@naver.com>
2024-04-03 17:20:52 +0900

    feat: 발급 할 Passport generator 클래스 구현
    연관 이슈: #31

---
+++ src/auth-service/src/main/java/com/phishing/authservice/component/passport/PassportGenerator.java
@@ -0,0 +1,15 @@
+package com.phishing.authservice.component.passport;
+
+import com.phishing.authservice.domain.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PassportGenerator {
+        public Passport generatePassport(User user){
+            return new Passport(
+                    user.getEmail(),
+                    user.getNickname(),
+                    user.getRole().toString()
+            );
+        }
+}

055262fd3ed83cff1704f1494cd65e95eae690dc
sso9594 <sso9594@naver.com>
2024-04-03 17:17:56 +0900

    feat: service passport 발급 로직 추가
    연관 이슈: #31

--- src/auth-service/src/main/java/com/phishing/authservice/service/AuthService.java
+++ src/auth-service/src/main/java/com/phishing/authservice/service/AuthService.java
@@ -1,11 +1,16 @@
 package com.phishing.authservice.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.phishing.authservice.component.passport.Passport;
+import com.phishing.authservice.component.passport.PassportGenerator;
 import com.phishing.authservice.component.token.ReturnToken;
 import com.phishing.authservice.component.token.TokenProvider;
 import com.phishing.authservice.component.token.TokenResolver;
 import com.phishing.authservice.domain.User;
 import com.phishing.authservice.payload.request.SignInRequest;
 import com.phishing.authservice.exception.exceptions.InvalidPasswordException;
+import com.phishing.authservice.payload.token.MemberInfo;
 import com.phishing.authservice.redis.RedisDao;
 import com.phishing.authservice.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
@@ -29,9 +34,14 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
     private final TokenResolver tokenResolver;
+    private final PassportGenerator passportGenerator;
+    private final ObjectMapper objectMapper;
 
     @Value("${jwt.secret.refresh}")
     private Long refreshTime;
+
+    private static final String ACCESS_TOKEN_HEADER = "Authorization";
+    private static final String REFRESH_TOKEN_HEADER = "RefreshToken";
 
     public ReturnToken signIn(SignInRequest request) {
         userRepository.existsByEmail(request.email());
@@ -46,8 +56,8 @@ public class AuthService {
     }
 
     public void signOut(HttpServletRequest request) {
-        String accessToken = request.getHeader("Authorization");
-        String refreshToken = request.getHeader("RefreshToken");
+        String accessToken = request.getHeader(ACCESS_TOKEN_HEADER);
+        String refreshToken = request.getHeader(REFRESH_TOKEN_HEADER);
         long remainTime = tokenResolver.getExpiration(refreshToken);
         long ttl = remainTime - System.currentTimeMillis();
         String email = tokenResolver.getAccessClaims(accessToken).email();
@@ -58,7 +68,7 @@ public class AuthService {
     }
 
     public ReturnToken refresh(HttpServletRequest request) {
-        String refreshToken = request.getHeader("RefreshToken");
+        String refreshToken = request.getHeader(REFRESH_TOKEN_HEADER);
         String email = tokenResolver.getRefreshClaims(refreshToken).email();
         if (!redisDao.isExistKey(email) || !redisDao.getRedisValues(email).equals(refreshToken)) {
             throw new InvalidPasswordException("Invalid refresh token");
@@ -70,5 +80,16 @@ public class AuthService {
                 returnToken.refreshToken(), Duration.ofMillis(refreshTime));
 
         return returnToken;
+    }
+
+    public String generatePassport(HttpServletRequest request) throws JsonProcessingException {
+        String accessToken = request.getHeader(ACCESS_TOKEN_HEADER);
+        String user_email = tokenResolver.getAccessClaims(accessToken).email();
+        User user = userRepository.findByEmailAndIsDeletedIsFalse(user_email)
+                .orElseThrow(() -> new InvalidPasswordException("Invalid access token"));
+        Passport result = passportGenerator.generatePassport(user);
+        log.debug("Passport: {}", result);
+        String passport = objectMapper.writeValueAsString(result);
+        return passport;
     }
 }

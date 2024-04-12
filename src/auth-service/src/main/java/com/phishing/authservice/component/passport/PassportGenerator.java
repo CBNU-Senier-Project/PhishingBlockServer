60324ce6a0ffb0971047aa6680b7e9cba3349d01
sso9594 <sso9594@naver.com>
2024-04-12 15:31:25 +0900

    feat: Passport에 userId 추가
    연관 이슈: #31

--- src/auth-service/src/main/java/com/phishing/authservice/component/passport/PassportGenerator.java
+++ src/auth-service/src/main/java/com/phishing/authservice/component/passport/PassportGenerator.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 public class PassportGenerator {
         public Passport generatePassport(User user){
             return new Passport(
+                    user.getId(),
                     user.getEmail(),
                     user.getNickname(),
                     user.getRole().toString()
